dc5d6ae99a5cebb3896f165cddb217a781cd27f9
Arturo Diaz <6arturodiaz@gmail.com>
2025-05-19 14:29:15 -0700

    Add auto scrolltotop for react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop"; // Adjust the path if necessary
 import SearchPage from "./SearchPage";
 import BuyLiveRent from "./pages/BuyLiveRent";
 import BuyRent from "./pages/BuyRent";
@@ -12,6 +13,7 @@ import Commercial from "./pages/Commercial";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/property/:id/buy_and_Live_and_rent" element={<BuyLiveRent />} />
